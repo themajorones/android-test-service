dae26c0d8f4414c94fa88e6c7fa6dc27cf2770d4
iamSlightlyWind <iam.slightlywind@themajorones.dev>
2026-04-27 12:51:45 +0700

    tree(prj): init

---
+++ src/main/java/dev/themajorones/autotest/AutotestApplication.java
@@ -0,0 +1,13 @@
+package dev.themajorones.autotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutotestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/themajorones/autotest/AutotestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.themajorones.autotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
